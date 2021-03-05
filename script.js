c665244010cce3e985a0895e3c81284f37d2f491
Josef Zacek <joezacek@gmail.com>
2021-03-05 17:34:52 +0000

    added page progress bar js

---
+++ script.js
@@ -0,0 +1,11 @@
+// page progress bar
+function pageScroll() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("page-progress-bar-line").style.width = scrolled + "%";
+}
+
+window.onscroll = function(){
+  pageScroll()
+};
