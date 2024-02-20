27e7e00f384fd55f17cdbe6f8bd753cb38e349d5
Josef Zacek <joezacek@gmail.com>
2024-02-20 10:54:42 +0000

    scroll to section without changing url

--- script.js
+++ script.js
@@ -10,4 +10,12 @@ window.onscroll = function(){
   pageScroll()
 };
 
+// scroll to anchor
+var navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach(function(link) {
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    var sectionName = this.getAttribute('href').substring(1);
+    document.getElementById(sectionName).scrollIntoView(true);
+  });
 });
